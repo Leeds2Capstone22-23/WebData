52326f570244f359ad810468343a4d964bdd3165
EmilyParker24 <parkeremily24@gmail.com>
2022-10-27 13:22:48 -0600

    Get other tweet info (buggy still)

--- twitter/post.js
+++ twitter/post.js
@@ -22,12 +22,91 @@ async function getPost(url, getReplies) {
       return x;
     });
 
-    // TODO: get user, likes, retweets, etc count
+    const results = await page.$$eval("article ", (tweets) => {
+      if (!tweets || tweets == null) return [];
+      tweets = tweets.splice(1, tweets.length);
+      return tweets.map((tweet) => {
+        try {
+          if (!tweet || tweet == null) return 0;
+          if (
+            !tweet.querySelectorAll("time") ||
+            tweet.querySelectorAll("time") == null ||
+            !tweet.outerHTML
+          )
+            return 0;
 
+          //fallback mechanism to enter null if selector cannot retreive
+
+          let _published = null;
+          // let _text = null;
+          let _replies = null;
+          let _retweets = null;
+          let _likes = null;
+          let _error = null;
+          let _url = [...tweet.querySelectorAll("a")]
+            .map((e) => e.getAttribute("href"))
+            .filter((e) => e.includes("status") && !e.includes("photo"))[0];
+
+          try {
+            //find tweetmap data
+
+            _published = tweet
+              .querySelectorAll("time")[0]
+              .getAttribute("datetime");
+            // _text = tweet.querySelectorAll("div[lang]").textContent
+            console.log(_text)
+            if (tweet.outerHTML.match("[0-9]+ .etweets"))
+              _retweets = tweet.outerHTML
+                .match("[0-9]+ .etweets")[0]
+                .split(" ")[0];
+            else
+              _retweets = tweet.outerHTML
+                .match("[0-9]+ .etweet")[0]
+                .split(" ")[0];
+            if (tweet.outerHTML.match("[0-9]+ .ikes"))
+              _likes = tweet.outerHTML.match("[0-9]+ .ikes")[0].split(" ")[0];
+            else
+              _likes = tweet.outerHTML.match("[0-9]+ .ike")[0].split(" ")[0];
+            if (tweet.outerHTML.match("[0-9]+ .eplies"))
+              _replies = tweet.outerHTML
+                .match("[0-9]+ .eplies")[0]
+                .split(" ")[0];
+            else
+              _replies = tweet.outerHTML
+                .match("[0-9]+ .eply")[0]
+                .split(" ")[0];
+          } catch (ex) {
+            _error = ex.toString();
+          }
+          //populate tweetmap
+
+          return {
+            url: _url,
+            // text: _text,
+            published: _published,
+            replies: _replies,
+            retweets: _retweets,
+            likes: _likes,
+            error: _error,
+          };
+        } catch (e) {
+          console.log("puppeteer error");
+          console.log(e);
+          return 0;
+        }
+      });
+    });
+
+    let subTweetUrls = []
+    subTweetUrls.push(...results.filter((e) => e !== 0));
+    tweet = subTweetUrls[0]
+    tweet.text = text
+
+    
     // TODO: handle if get replies = True
 
     browser.close()
-    return text
+    return tweet
 }
 
 
